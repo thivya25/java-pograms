99a28d9f8b679627034ad71dbce82228f85cf9ef
thivya25 <60476179+thivya25@users.noreply.github.com>
2023-09-26 22:31:04 +0530

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,50 +1,81 @@
-package mainapppackage1;
+package com.edu;
 
-import mainapppackage2.HR;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
 
-//default, protected, public
-class Employee{
-		private int i;
+public class MainApp {
+
+	public static void main(String[] args) {
+	 String driver="com.mysql.cj.jdbc.Driver";
+		 String url="jdbc:mysql://localhost:3306/thivyadb";
+		 String un="root";
+		 String pass="toor";
+		 Connection conn=null;
+		 PreparedStatement pst=null;
+		 ResultSet rs=null;
+		 int eid=0,eage=0,edid=0;
+		 String ename=null,email=null;
+		 float esal=0;
+		 Scanner sc=new Scanner(System.in);
+		 
+		 String genid="select max(eid)+1 as empid from employee";
 		
-		private void empPersonalDetails() {
-			System.out.println("empPersonalDetails");
-		}
-		protected void display() {
-			System.out.println("display method "+i);
-			empPersonalDetails();
-		}
-}
-class Employees extends HR
-{
-	private int i;
-	
-	private void empPersonalDetails()
-	{
-		System.out.println("empPersonalDetails");
-	}
-	public void display() 
-	{
-		System.out.println("display method "+id);
-		System.out.println("display method "+name);
-		
-		empPersonalDetails();
-	}
-}
-
-
-	public class MainApp {
-
-		public static void main(String[] args) 
-		{
-			Employees eob = new Employees();
-			eob.input();
-			eob.display();
+		try {
+			Class.forName(driver);
+			conn=DriverManager.getConnection(url,un,pass);
+			pst=conn.prepareStatement(genid);
+			rs=pst.executeQuery();
 			
-//			HR hob=new HR();
-//			hob.input();
-//		     hob.display();
+			if(rs.next()) {
+				eid=rs.getInt("empid");//3
+				System.out.println("Employee id="+eid);
+			}
+			System.out.println("Enter name");
+			ename=sc.next();
+			System.out.println("Enter emailid");
+			email=sc.next();
+			System.out.println("Enter salary");
+			esal=sc.nextFloat();
+			System.out.println("Enter age");
+			eage=sc.nextInt();
+			System.out.println("Enter deptid");
+			edid=sc.nextInt();
 
+			
+			String ins="insert into employee(eid,ename,eage,esalary,eemail,deptid) values(?,?,?,?,?,?)";
+			pst=conn.prepareStatement(ins);
+			pst.setInt(1, eid);
+			pst.setString(2, ename);
+			pst.setInt(3, eage);
+			pst.setFloat(4, esal);
+			pst.setString(5, email); 
+			
+			String check ="select * from department where deptid=?";
+			PreparedStatement pst1=conn.prepareStatement(check);
+			pst1.setInt(1, edid);
+			ResultSet rs1=pst1.executeQuery();
+			if(rs1.next()) {
+				pst.setInt(6, edid);
+			
+			int i=pst.executeUpdate();
+			if(i>0) {
+				System.out.println("Employee added");
+			}
+			else {
+				System.out.println("Error");
+			}
+			}else {
+				System.out.println("depatment not found");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-		
+
+	}
 
 }
+
+
